b3b819d78cb01fe0863d1fd0ca0e91a12b90196a
LAPTOP WA <LAPTOP WA@DESKTOP-Q5L0P7T>
2024-11-05 08:40:50 +0700

    refangga first commit

--- src/database/database.java
+++ src/database/database.java
@@ -3,23 +3,20 @@ package database;
 import java.sql.*;
 
 public class database {
-    private static final String URL = "jdbc:mysql://localhost:3306/app_rental";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+   public static Connection com;
+    public static Statement stm; 
 
-    public static Connection getConnection() {
-        Connection connection = null;
+    public static void main(String[] args) {
         try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Koneksi ke database berhasil!");
-        } catch (SQLException e) {
-            System.out.println("Koneksi ke database gagal!");
-            e.printStackTrace();
+            String url = "jdbc:mysql://localhost:3306/app_rental";
+            String user = "root";
+            String pass = "";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            com =DriverManager.getConnection(url,user,pass);
+            stm =com.createStatement();
+            System.out.println("Koneksi berhasil");
+        } catch (Exception e) {
+            System.err.println("Koneksi gagal" +e.getMessage());
         }
-      
-        return connection;
-        
-    } public static void main(String[] args) {
-        database.getConnection();
     }
 }
